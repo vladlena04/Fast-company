42a3d4b29363fe3a99c180a2ace3478bc795faac
V.Bonk <eltarya@gmail.com>
2022-02-27 16:05:23 +0300

    pagination

---
+++ fast-company/src/app/components/bookmark.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const Bookmark = ({ status, ...rest }) => {
+    return (
+        <button {...rest}>
+            <i className={"bi bi-bookmark" + (status ? "-heart-fill" : "")}></i>
+        </button>
+    );
+};
+
+Bookmark.propTypes = {
+    status: PropTypes.bool.isRequired
+};
+
+export default BookMark;
---
+++ fast-company/src/app/components/users.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import { paginate } from "../utils/paginate";
+import Pagination from "./pagination";
+import User from "./user";
+const Users = ({ users, ...rest }) => {
+    const count = users.length
+    const pageSize = 4
+    const [currentPage, setCurrentPage]=useState(1)
+    const handlePageChange = (pageIndex) => {
+        setCurrentPage(pageIndex)
+    }
+    
+    const userCrop = paginate(users, currentPage, pageSize)
+    return (
+        <>
+            {count > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Профессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th scope="col">Избранное</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {userCrop.map((user) => (
+                            <User {...rest} {...user} key={user._id} />
+                        ))}
+                    </tbody>
+                </table>
+            )}
+            <Pagination 
+                itemsCount={count} 
+                pageSize={pageSize} 
+                currentPage={currentPage}
+                onPageChange={handlePageChange}/>
+        </>
+    );
+};
+
+export default Users;
