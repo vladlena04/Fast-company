d7b4d8a313d9b69fe7ee900c9fdbe0c9899cc00e
V.Bonk <eltarya@gmail.com>
2022-04-14 22:40:19 +0300

    add new ui

--- fast-company/src/app/layouts/users.jsx
+++ fast-company/src/app/layouts/users.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import EditUserPage from "../components/page/userPage";
+import EditUserPage from "../components/page/editUserPage";
 import UserPage from "../components/page/userPage";
 import UsersListPage from "../components/page/usersListPage";
 const Users = () => {
     const params = useParams();
     const { userId, edit } = params;
-    return (<>{userId ? (
-        edit ? (
-            <EditUserPage />
-        ) : (
-            <UserPage userId={userId} />
-        )
-    ) : ( 
-        <UsersListPage />)}
-    </>
+    return (
+        <>
+            {userId ? (
+                edit ? (
+                    <EditUserPage />
+                ) : (
+                    <UserPage userId={userId} />
+                )
+            ) : (
+                <UsersListPage />
+            )}
+        </>
     );
 };
 
